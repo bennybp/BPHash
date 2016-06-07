363254ec5010456ac453d65c3a4065aa59a97a69
Ben Pritchard <ben@bennyp.org>
2016-06-07 13:00:13 -0400

    Better installation. Add StdHash

---
+++ bphash/StdHash.hpp
@@ -0,0 +1,23 @@
+#ifndef BPHASH_STDHASH_HPP
+#define BPHASH_STDHASH_HPP
+
+#include "bphash/Hasher.hpp"
+
+namespace bphash {
+
+/*! \brief A class that can be used in place of std::hash in containers */
+template<typename T>
+struct StdHash
+{
+    size_t operator()(const T & obj) const
+    {
+        //! \todo replace with Hash64 if available?
+        Hasher h(HashType::Hash128);
+        h(obj);
+        return h.finalize().truncate<size_t>();
+    }
+};
+
+}
+
+#endif
